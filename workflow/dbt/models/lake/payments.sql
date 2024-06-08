e2760ea9682d0eb60e1d622ffb95f57dd41e9650
kf3225 <k.fujita3225@gmail.com>
2024-06-08 14:49:37 +0900

    add partition to raw data

--- workflow/dbt/models/lake/payments.sql
+++ workflow/dbt/models/lake/payments.sql
@@ -4,6 +4,8 @@ SELECT
     CAST(id AS VARCHAR) AS id,
     CAST(order_id AS VARCHAR) AS order_id,
     CAST(payment_method AS VARCHAR) AS payment_method,
-    CAST(amount AS INTEGER) AS amount
+    CAST(amount AS INTEGER) AS amount,
+    CAST(year AS VARCHAR) AS year,
+    CAST(month AS VARCHAR) AS month
 FROM
     {{ source('external_source', 'payments') }}
